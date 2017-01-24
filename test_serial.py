26ef39d83b27e44908326b81ad30786433d3a496
Peter Mitrano <mitranopeter@gmail.com>
2017-01-24 00:24:24 -0500

    remove shit serial program

--- test_serial.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3.5
-from time import sleep
-import serial
-
-with serial.Serial('/dev/ttyUSB0', 115200) as ser:
-    print(ser)
-    while True:
-        ser.write(b"hello")
-        print(ser.readline()[:-1].decode())
