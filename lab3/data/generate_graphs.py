7c444c693d5f843d22ca2b1bd26558dc8629bec6
Peter Mitrano <mitranopeter@gmail.com>
2017-02-14 16:38:19 -0500

    did postlab 5

--- lab3/data/generate_graphs.py
+++ lab3/data/generate_graphs.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas
 
+# histrograms of delta encoder counts
 for i, ext in enumerate(['3', '6', '-3']):
     f = open('./encoder_ticks_%s.csv'%ext)
 
@@ -13,13 +14,10 @@ for i, ext in enumerate(['3', '6', '-3']):
         datum = int(line.strip("\n"))
         if j > 0:
             diff = last_datum - int(datum)
-            if i == 3:
-                print(diff)
             differences.append(diff)
         last_datum = int(datum)
 
     plt.figure(i)
-    print(i, np.amax(differences))
     bins = list(np.arange(-11.5,11.5))
     plt.hist(differences, bins)
     plt.xlabel("Count difference")
@@ -27,5 +25,29 @@ for i, ext in enumerate(['3', '6', '-3']):
     plt.title("Change in Encoder Ticks at %s volts"%ext)
     plt.savefig('encoder_ticks_%s.png' % ext)
 
+# histograms of accelerometer data
+plt.subplot(2,3,1)
+data = pandas.read_csv('10sec_horizontal.csv')
+data['ax'].hist(bins=8)
+plt.title('Horizontal X Acceleration')
+plt.subplot(2,3,2)
+data['ay'].hist(bins=8)
+plt.title('Horizontal Y Acceleration')
+plt.subplot(2,3,3)
+data['az'].hist(bins=8)
+plt.title('Horizontal Z Acceleration')
+
+plt.subplot(2,3,4)
+data = pandas.read_csv('10sec_vertical.csv')
+data['ax'].hist(bins=8)
+plt.title('Vertical X Acceleration')
+plt.subplot(2,3,5)
+data['ay'].hist(bins=8)
+plt.title('Vertical Y Acceleration')
+plt.subplot(2,3,6)
+data['az'].hist(bins=8)
+plt.title('Vertical Z Acceleration')
+
+# graphs of position/velocity/acceleration
 plt.show()
 
